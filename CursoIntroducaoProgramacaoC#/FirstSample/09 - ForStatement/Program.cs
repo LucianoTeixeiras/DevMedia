95e6ad701b12785cd3e94c48ba76662a77f117e5
Luciano Teixeira <luciano.teixeiras@gmail.com>
2017-01-05 16:31:11 -0200

    Reveuw

---
+++ CursoIntroducaoProgramacaoC#/FirstSample/09 - ForStatement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09___ForStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
