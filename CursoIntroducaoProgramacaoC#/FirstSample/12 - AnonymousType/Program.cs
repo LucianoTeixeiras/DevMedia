dc8ac623914c530399eb6801edc23abde340fe79
Luciano Teixeira <luciano.teixeiras@gmail.com>
2017-01-06 14:50:03 -0200

    Logics Operators

---
+++ CursoIntroducaoProgramacaoC#/FirstSample/12 - AnonymousType/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12___AnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ
+
+            //Nullable: permite nulidade na declaraçãod a variavel. para type int a sintaxy é: int? pas sting nao é necessario pois a nulidade é nativa.
+            int? i = null;
+            string s = null;
+
+            var valores = new[] { new { idade = 18, nome = "Luis" }, new { idade = 28, nome = "Laura" } };
+
+            foreach (var item in valores)
+            {
+                Console.WriteLine("Idade: " + item.idade.ToString());
+                Console.WriteLine("Nome: " + item.nome.ToString());
+                Console.WriteLine("------------------------");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CursoIntroducaoProgramacaoC#/FirstSample/13 - Operators/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13___Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores Aritmeticos
+
+            int a = 10;
+            int b = 2;
+
+            //Add
+
+            int add = a + b;
+
+            //Sub
+
+            int sub = a - b;
+
+            //Mult
+
+            int mult = a * b;
+
+            //Div
+
+            int div = a / b;
+
+            //Resto
+
+            int rest = 5 % 2;
+
+            //Operadores Primarios
+
+            //Incremento:   x++
+            //Decremento:   x--
+
+            //Operadores Comparativos
+
+            //Diferenca
+            if (a > b)
+            {
+                Console.WriteLine("A é maior que B");
+            }
+            else if (b < a)
+            {
+                Console.WriteLine("B é menor que A");
+            }
+            else if (a >= b)// Lambda Expression
+            {
+                Console.WriteLine("A é maior/igual que B");
+            }
+            else if (b <=a )
+            {
+                Console.WriteLine("B é menor/igual que A");
+            }
+            //Equals
+            else if (a==b)
+            {
+                Console.WriteLine("B é igual a A");
+            }
+            //Not
+            else if (a!=b)
+            {
+                Console.WriteLine("B diferente de A");
+            }
+
+            //Comparadores Logicos
+
+            //XOR:  Lógico Exclusivo - Ou um Ou outro
+
+            if (true ^ false)
+            {
+                Console.WriteLine("True");
+            }
+
+            //Comparadores Condicionais
+
+            //AND
+
+            if (true && true)
+            {
+                Console.WriteLine("True");
+            }
+
+            //Or
+            if (true || false)
+            {
+                Console.WriteLine("True");
+            }
+
+            Console.WriteLine("O Resultado da Add e: " + add);
+            Console.WriteLine("O Resultado da Sub e: " + sub);
+            Console.WriteLine("O Resultado da Mult e: " + mult);
+            Console.WriteLine("O Resultado da Div e: " + div);
+            Console.WriteLine("O Resto da Divisao e: " + rest);
+            Console.ReadKey();
+        }
+    }
+}
